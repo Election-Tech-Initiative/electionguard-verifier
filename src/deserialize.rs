86cf54300ead813c1dd58d37756299f1b7559a77
Jake Waksbaum <jake.waksbaum@gmail.com>
2019-08-13 16:44:06 -0700

    Fix biguint deserialize by using u64

--- src/deserialize.rs
+++ src/deserialize.rs
@@ -1,10 +1,10 @@
-use num::{BigUint, FromPrimitive};
-use serde::{de, Deserialize, Deserializer};
+use num::BigUint;
+use serde::{Deserialize, Deserializer};
 
 pub fn biguint<'de, D>(deserializer: D) -> Result<BigUint, D::Error>
 where
     D: Deserializer<'de>,
 {
-    let n = <u128 as Deserialize>::deserialize(deserializer)?;
-    BigUint::from_u128(n).ok_or(de::Error::custom("Unable to serialize BigUint from value"))
+    let n: u64 = Deserialize::deserialize(deserializer)?;
+    Ok(From::from(n))
 }
