91c53101b9d9b5a5021406c244483cd11eeed075
Jake Waksbaum <jake.waksbaum@gmail.com>
2019-08-19 11:23:52 -0700

    Finish verification of disj. of CP proofs

--- src/crypto/chaum_pederson.rs
+++ src/crypto/chaum_pederson.rs
@@ -1,8 +1,10 @@
+use digest::Digest;
 use num::BigUint;
 use serde::{Deserialize, Serialize};
+use sha2::Sha256;
 use std::iter;
 
-use super::elgamal::{Group, Message};
+use super::elgamal::{self, Group, Message};
 
 /// A proof that the sum of the selections is equal to `L`, by proving
 /// that their difference is zero.
@@ -42,8 +44,76 @@ pub struct Proof {
     response: BigUint,
 }
 
+#[derive(Debug)]
 pub enum Error {
     ResponsePublicKey,
+    ResponseCiphertext,
+    Challenge,
+}
+
+impl Proof {
+    pub fn verify_disj<'a>(
+        p: &'a Self,
+        q: &'a Self,
+        group: &'a Group,
+        message: &'a Message,
+        public_key: &'a BigUint,
+        hash_order: &[DisjHashInput<impl AsRef<[u8]>>],
+    ) -> impl Iterator<Item = Error> + 'a {
+        iter::empty()
+            .chain(p.verify_response(group, message, public_key))
+            .chain(q.verify_response(group, message, public_key))
+            .chain(Self::verify_disj_hash(p, q, group, message, hash_order))
+    }
+
+    fn verify_disj_hash<'a>(
+        p: &'a Self,
+        q: &'a Self,
+        group: &'a Group,
+        message: &'a Message,
+        hash_order: &[DisjHashInput<impl AsRef<[u8]>>],
+    ) -> Option<Error> {
+        let digest: Sha256 = Digest::new();
+        let digest = hash_order
+            .iter()
+            .fold(digest, |d, x| x.chain(d, p, q, message));
+        let hash = digest.result();
+
+        if BigUint::from_bytes_be(hash.as_slice()) == (&p.challenge + &q.challenge) % &group.prime {
+            None
+        } else {
+            Some(Error::Challenge)
+        }
+    }
+}
+
+pub enum DisjHashInput<C> {
+    Message,
+    LeftCommittment,
+    RightCommittment,
+    Custom(C),
+}
+
+impl<C> DisjHashInput<C> {
+    fn chain<D>(&self, digest: D, p: &Proof, q: &Proof, message: &Message) -> D
+    where
+        D: Digest,
+        C: AsRef<[u8]>,
+    {
+        use DisjHashInput::*;
+
+        let chain_message = |d: D, message: &elgamal::Message| {
+            let d = d.chain(message.public_key.to_bytes_be());
+            d.chain(message.ciphertext.to_bytes_be())
+        };
+
+        match self {
+            Message => chain_message(digest, message),
+            LeftCommittment => chain_message(digest, &p.committment),
+            RightCommittment => chain_message(digest, &q.committment),
+            Custom(input) => digest.chain(input),
+        }
+    }
 }
 
 impl Proof {
@@ -69,13 +139,7 @@ impl Proof {
 
     /// Verify that the first component of the response (the one-time public key) is correct:
     /// `gᵘ = α aᶜ`.
-    ///
-    /// # Parameters
-    /// - `group`: the ElGamal group for the message, `(p, g)`
-    /// - `message`: the message we want to prove encodes zero, `(a,
-    ///    b) = (gʳ, m hʳ)`, where `r` is the private key used to
-    ///    encrypt the message.
-    /// - `public_key`: the public key `h` used to encrypt the message
+    #[allow(clippy::many_single_char_names)]
     fn verify_response_public_key(&self, group: &Group, message: &Message) -> Option<Error> {
         let Group {
             generator: g,
@@ -99,13 +163,7 @@ impl Proof {
 
     /// Verify that the second component of the response (the ciphertext) is correct:
     /// `hᵘ = β bᶜ`.
-    ///
-    /// # Parameters
-    /// - `group`: the ElGamal group for the message, `(p, g)`
-    /// - `message`: the message we want to prove encodes zero, `(a,
-    ///    b) = (gʳ, m hʳ)`, where `r` is the private key used to
-    ///    encrypt the message.
-    /// - `public_key`: the public key `h` used to encrypt the message
+    #[allow(clippy::many_single_char_names)]
     fn verify_response_ciphertext(
         &self,
         group: &Group,
@@ -126,7 +184,7 @@ impl Proof {
         if BigUint::modpow(h, u, p) == (beta * BigUint::modpow(b, c, p)) % p {
             None
         } else {
-            Some(Error::ResponsePublicKey)
+            Some(Error::ResponseCiphertext)
         }
     }
 }
