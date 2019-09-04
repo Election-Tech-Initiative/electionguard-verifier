97590fb375128e4d885c96ce33a629a534b348fb
Stuart Pernsteiner <spernsteiner@galois.com>
2019-09-04 15:25:32 -0700

    functions for constructing chaum-pedersen disjunction proofs

--- src/test.rs
+++ src/test.rs
@@ -346,6 +346,88 @@ impl chaum_pederson::Proof {
     }
 }
 
+impl chaum_pederson::disj::Proof {
+    /// Given a `message` that's an encryption of zero, construct a proof that it's either an
+    /// encryption of zero or an encryption of one.
+    pub fn prove_zero(
+        group: &Group,
+        public_key: &BigUint,
+        message: &Message,
+        one_time_secret: &BigUint,
+        real_one_time_exponent: &BigUint,
+        fake_challenge: &BigUint,
+        fake_response: &BigUint,
+        gen_challenge: impl FnOnce(&Message, &Message) -> BigUint,
+    ) -> chaum_pederson::disj::Proof {
+        let proof_one = chaum_pederson::Proof::simulate_plaintext(
+            group,
+            public_key,
+            message,
+            &1_u8.into(),
+            fake_challenge,
+            fake_response,
+        );
+        let proof_zero = chaum_pederson::Proof::prove_plaintext(
+            group,
+            public_key,
+            message,
+            one_time_secret,
+            &0_u8.into(),
+            real_one_time_exponent,
+            |commitment_zero| {
+                let combined_challenge = gen_challenge(commitment_zero, &proof_one.committment);
+                mod_sub(&combined_challenge, fake_challenge, &(&group.prime - 1_u8))
+            },
+        );
+        chaum_pederson::disj::Proof {
+            left: proof_zero,
+            right: proof_one,
+        }
+    }
+
+    /// Given a `message` that's an encryption of one, construct a proof that it's either an
+    /// encryption of zero or an encryption of one.
+    pub fn prove_one(
+        group: &Group,
+        public_key: &BigUint,
+        message: &Message,
+        one_time_secret: &BigUint,
+        real_one_time_exponent: &BigUint,
+        fake_challenge: &BigUint,
+        fake_response: &BigUint,
+        gen_challenge: impl FnOnce(&Message, &Message) -> BigUint,
+    ) -> chaum_pederson::disj::Proof {
+        let proof_zero = chaum_pederson::Proof::simulate_plaintext(
+            group,
+            public_key,
+            message,
+            &0_u8.into(),
+            fake_challenge,
+            fake_response,
+        );
+        let proof_one = chaum_pederson::Proof::prove_plaintext(
+            group,
+            public_key,
+            message,
+            one_time_secret,
+            &1_u8.into(),
+            real_one_time_exponent,
+            |commitment_one| {
+                let combined_challenge = gen_challenge(&proof_zero.committment, commitment_one);
+                mod_sub(&combined_challenge, fake_challenge, &(&group.prime - 1_u8))
+            },
+        );
+        chaum_pederson::disj::Proof {
+            left: proof_zero,
+            right: proof_one,
+        }
+    }
+
+    pub fn challenge(&self, group: &Group) -> BigUint {
+        (&self.left.challenge + &self.right.challenge) % &(&group.prime - 1_u8)
+    }
+}
+
 
 /// Encrypt a zero, construct a Chaum-Pederson proof that it's zero, and check the proof.
 #[test]
@@ -682,4 +764,148 @@ fn simulate_check_plaintext() {
     let status = proof.check_plaintext(&group, &public_key, &message, &plaintext);
     dbg!(&status);
     assert!(status.is_ok());
+}
+
+/// Encrypt the value zero, prove that it's either zero or one, and check both parts of the proof.
+#[test]
+fn prove_check_disj_zero() {
+    let group = elgamal::test::group();
+    let public_key = elgamal::test::public_key();
+    let extended_base_hash = elgamal::test::extended_base_hash();
+
+    let one_time_secret = 8768_u32.into();
+    let message = Message::encrypt(&group, &public_key, &0_u8.into(), &one_time_secret);
+    let real_one_time_exponent = 24256_u32.into();
+    let fake_challenge = 30125_u32.into();
+    let fake_response = 6033_u32.into();
+    let proof = chaum_pederson::disj::Proof::prove_zero(
+        &group,
+        &public_key,
+        &message,
+        &one_time_secret,
+        &real_one_time_exponent,
+        &fake_challenge,
+        &fake_response,
+        |comm0, comm1| hash_uints(&[
+            &extended_base_hash,
+            &comm0.public_key, &comm0.ciphertext,
+            &comm1.public_key, &comm1.ciphertext,
+        ]),
+    );
+
+    let status0 = proof.left.check_plaintext(&group, &public_key, &message, &0_u8.into());
+    let status1 = proof.right.check_plaintext(&group, &public_key, &message, &1_u8.into());
+    dbg!(&status0);
+    dbg!(&status1);
+    assert!(status0.is_ok());
+    assert!(status1.is_ok());
+}
+
+/// Encrypt the value one, prove that it's either zero or one, and check both parts of the proof.
+#[test]
+fn prove_check_disj_one() {
+    let group = elgamal::test::group();
+    let public_key = elgamal::test::public_key();
+    let extended_base_hash = elgamal::test::extended_base_hash();
+
+    let one_time_secret = 8768_u32.into();
+    let message = Message::encrypt(&group, &public_key, &1_u8.into(), &one_time_secret);
+    let real_one_time_exponent = 24256_u32.into();
+    let fake_challenge = 30125_u32.into();
+    let fake_response = 6033_u32.into();
+    let proof = chaum_pederson::disj::Proof::prove_one(
+        &group,
+        &public_key,
+        &message,
+        &one_time_secret,
+        &real_one_time_exponent,
+        &fake_challenge,
+        &fake_response,
+        |comm0, comm1| hash_uints(&[
+            &extended_base_hash,
+            &comm0.public_key, &comm0.ciphertext,
+            &comm1.public_key, &comm1.ciphertext,
+        ]),
+    );
+
+    let status0 = proof.left.check_plaintext(&group, &public_key, &message, &0_u8.into());
+    let status1 = proof.right.check_plaintext(&group, &public_key, &message, &1_u8.into());
+    dbg!(&status0);
+    dbg!(&status1);
+    assert!(status0.is_ok());
+    assert!(status1.is_ok());
+}
+
+/// Encrypt the value two, construct a proof that falsely claims it's either zero or one, and check
+/// both parts of the proof (which should fail).
+#[test]
+#[should_panic]
+fn prove_check_disj_two() {
+    let group = elgamal::test::group();
+    let public_key = elgamal::test::public_key();
+    let extended_base_hash = elgamal::test::extended_base_hash();
+
+    let one_time_secret = 8768_u32.into();
+    let message = Message::encrypt(&group, &public_key, &2_u8.into(), &one_time_secret);
+    let real_one_time_exponent = 24256_u32.into();
+    let fake_challenge = 30125_u32.into();
+    let fake_response = 6033_u32.into();
+    let proof = chaum_pederson::disj::Proof::prove_zero(
+        &group,
+        &public_key,
+        &message,
+        &one_time_secret,
+        &real_one_time_exponent,
+        &fake_challenge,
+        &fake_response,
+        |comm0, comm1| hash_uints(&[
+            &extended_base_hash,
+            &comm0.public_key, &comm0.ciphertext,
+            &comm1.public_key, &comm1.ciphertext,
+        ]),
+    );
+
+    let status0 = proof.left.check_plaintext(&group, &public_key, &message, &0_u8.into());
+    let status1 = proof.right.check_plaintext(&group, &public_key, &message, &1_u8.into());
+    dbg!(&status0);
+    dbg!(&status1);
+    assert!(status0.is_ok());
+    assert!(status1.is_ok());
+}
+
+/// Encrypt the value zero, wrongly construct a proof using `prove_one`, and check both parts of
+/// the proof (which should fail).
+#[test]
+#[should_panic]
+fn prove_check_disj_one_flipped() {
+    let group = elgamal::test::group();
+    let public_key = elgamal::test::public_key();
+    let extended_base_hash = elgamal::test::extended_base_hash();
+
+    let one_time_secret = 8768_u32.into();
+    let message = Message::encrypt(&group, &public_key, &0_u8.into(), &one_time_secret);
+    let real_one_time_exponent = 24256_u32.into();
+    let fake_challenge = 30125_u32.into();
+    let fake_response = 6033_u32.into();
+    let proof = chaum_pederson::disj::Proof::prove_one(
+        &group,
+        &public_key,
+        &message,
+        &one_time_secret,
+        &real_one_time_exponent,
+        &fake_challenge,
+        &fake_response,
+        |comm0, comm1| hash_uints(&[
+            &extended_base_hash,
+            &comm0.public_key, &comm0.ciphertext,
+            &comm1.public_key, &comm1.ciphertext,
+        ]),
+    );
+
+    let status0 = proof.left.check_plaintext(&group, &public_key, &message, &0_u8.into());
+    let status1 = proof.right.check_plaintext(&group, &public_key, &message, &1_u8.into());
+    dbg!(&status0);
+    dbg!(&status1);
+    assert!(status0.is_ok());
+    assert!(status1.is_ok());
 }
